3bdb5c3b21004be865209540064cbd760d6b565f
Elliott.Evans <hello@wearebold.co.uk>
2023-06-27 21:55:18 +0100

    update(WIP Design)

--- site/pages/products/[…slug].vue
+++ site/pages/products/[…slug].vue
@@ -4,15 +4,18 @@
 const { path } = useRoute();
 const { data } = await useAsyncData(`content-${path}`, async () => {
   // fetch document where the document path matches with the cuurent route
-  let article = queryContent('products').where({ _path: path }).findOne();
+  let article = queryContent("products").where({ _path: path }).findOne();
   // get the surround information,
   // which is an array of documeents that come before and after the current document
-  let surround = queryContent('products').only(["_path", "title", "description"]).sort({ date: 1 }).findSurround(path);
+  let surround = queryContent("products")
+    .only(["_path", "title", "description"])
+    .sort({ date: 1 })
+    .findSurround(path);
 
   return {
     article: await article,
     models: await article.models,
-    surround: await surround,    
+    surround: await surround,
   };
 });
 
@@ -34,83 +37,83 @@ useHead({
 });
 </script>
 <template>
-  <main id="main" class="article-main">
-    <article>
-      <header v-if="data.article" class="article-header">    
-        <h1 class="heading">{{ data.article.title }}</h1>        
-        <ul class="article-tags">
-          <li class="tag !py-0.5" v-for="(tag, n) in data.article.category" :key="n">{{ tag }}</li>
-        </ul>  
+  <main class="">
+    <article class="l-container">
+      <header
+        class="grid grid-cols-1 l-grid-line l-grid-line--t md:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px"
+      >
+        <div class="hidden md:block md:col-span-2 bg-white p-4"></div>
+        <div
+          class="md:col-span-10 bg-b-blue-500 text-white"
+          style="padding: var(--space-xs)"
+        >
+          <h1 class=" font-medium leading-tight tracking-wide">
+            <span class="text-h1">{{ data.article.title }}</span><br>
+            <span class="text-h3" v-if="data.article.powerRange">{{ data.article.powerRange.from }} – {{ data.article.powerRange.to }} {{ data.article.powerRange.unit }}</span>
+          </h1>
+        </div>
       </header>
-  
-      <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
-        <section class="prose" aria-label="Description">
-          <p class="supporting">{{ data.article.description }}</p>
-        <h2>Key Features:</h2>
-          <ul class="list-disc">            
-              <li class="!py-0.5" v-for="(tag, n) in data.article.highlights" :key="n">{{ tag }}</li>
-          </ul>
-        </section>      
-        <section aria-label="Gallery">
-        <div class="relative w-full flex items-center gap-6 snap-x snap-mandatory overflow-x-auto pb-14 max-w-md overflow-hidden">
-          <div class="snap-center shrink-0 max-w-md mx-auto" v-for="(img, n) in data.article.images" :key="n">
-            <nuxt-img :src="`/img/products/${img}`" sizes="sm:100vw md:50vw lg:400px" :alt="data.article.title" class="" />
+      <section
+        class="grid grid-cols-1 l-grid-line l-grid-line--t md:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px"
+      >
+        <div class="hidden md:block md:col-span-2 bg-white"></div>
+        <div class="md:col-span-10 bg-white" style="padding: var(--space-xs)">
+          <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
+            <section class="prose" aria-label="Description">
+              <p class="text-h4 leading-tight">{{ data.article.description }}</p>
+              <h2>Key Features:</h2>
+              <ul class="list-disc">
+                <li
+                  class="!py-0.5"
+                  v-for="(tag, n) in data.article.highlights"
+                  :key="n"
+                >
+                  {{ tag }}
+                </li>
+              </ul>
+            </section>
+            <section aria-label="Gallery">
+              <div
+                class="relative w-full flex items-center gap-6 snap-x snap-mandatory overflow-x-auto pb-14 max-w-xl mx-auto overflow-hidden"
+              >
+                <div
+                  class="snap-center shrink-0 max-w-xl mx-auto"
+                  v-for="(img, n) in data.article.images"
+                  :key="n"
+                >
+                  <nuxt-img
+                    :src="`/img/products/${img}`"
+                    sizes="sm:100vw md:50vw lg:600px"
+                    :alt="data.article.title"
+                    class=""
+                  />
+                </div>
+              </div>
+            </section>
           </div>
+
+          <section class="prose" aria-label="Characteristics">
+            <h2>Characteristics</h2>
+            <p>text here</p>
+          </section>
+
+          <section class="prose" aria-label="Technical Specifications">
+            <h2>Technical Specifications</h2>
+            <ModelSelector :models="data.article.models"></ModelSelector>
+          </section>
+
+          <section aria-label="Downloads"></section>
+
+          <!-- PrevNext Component -->
+          <PrevNext :prev="prev" :next="next" />
+
+          <!-- <ul class="article-tags">
+          <li class="tag !py-0.5" v-for="(tag, n) in data.article.category" :key="n">{{ tag }}</li>
+        </ul>   -->
         </div>
       </section>
-      </div>      
-
-      <section class="prose" aria-label="Characteristics">
-        <h2>Characteristics</h2>
-        <p>text here</p>
-      </section>
-
-      <section class="prose" aria-label="Technical Specifications">
-      <h2>Technical Specifications</h2>
-      <ModelSelector :models="data.article.models"></ModelSelector>
-      </section>
-
-      <section aria-label="Downloads">
-
-      </section>
-
-      <!-- PrevNext Component -->
-      <PrevNext :prev="prev" :next="next" />
-
     </article>
   </main>
 </template>
 
-<style scoped>
-.article-main {
-  @apply p-4 max-w-5xl m-auto;
-}
-
-.article-header {
-  @apply p-4 pb-12;
-}
-
-.article-header .heading {
-  @apply font-extrabold text-5xl;
-}
-
-.article-header .supporting {
-  @apply font-medium text-lg;
-}
-
-.article-section {
-  @apply grid grid-cols-8;
-}
-
-.aside {
-  @apply col-span-full md:col-span-2 row-start-1 w-full pt-14;
-}
-
-.aside .toc {
-  @apply sticky top-20;
-}
-
-.article {
-  @apply col-span-full md:col-span-6 md:col-start-1 md:row-start-1 prose w-full p-4 max-w-3xl m-auto;
-}
-</style>
+<style scoped></style>
--- site/pages/products/index.vue
+++ site/pages/products/index.vue
@@ -12,55 +12,69 @@ useHead({
 });
 </script>
 <template>
-  <main>
-    <header class="page-heading">
-      <div class="wrapper">
-        <h1 class="text-5xl font-extrabold">All Products</h1>
-        <p class="font-medium text-lg">Here's a list of all my great products</p>
-      </div>
-    </header>
-    <section class="page-section">
-      <Category />
+     <main class="">
+      <article class="l-container">
+        <header class="grid grid-cols-1 l-grid-line l-grid-line--t md:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px">
+          <div class="hidden md:block md:col-span-2 bg-white p-4">
+          </div>
+          <div class="md:col-span-10 bg-b-blue-500 text-white" style="padding: var(--space-xs);">
+            <h1 class="text-h1 font-medium leading-tight tracking-wide">Products</h1>
+          </div>
+        </header>
+        <section class="grid grid-cols-1 l-grid-line l-grid-line--t lg:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px">
+          <aside class="lg:col-span-2 bg-white p-4">
+            <!-- <Category /> -->
+            Filters
+          </aside>
+          <div class="lg:col-span-10 bg-white">          
 
-      <!-- Render list of all articles in ./content/blog using `path` -->
-      <!-- Provide only defined fieldsin the `:query` prop -->
-      <ContentList
-        path="/product"
-        :query="{
-          only: ['title', 'description', 'category', '_path', 'highlights', 'powerRange', 'images'],
-          $sensitivity: 'base',
-        }"
-      >
-        <!-- Default list slot -->
-        <template v-slot="{ list }">
-          <ul class="article-list">
-            {{ article }}
-            <li v-for="article in list" :key="article._path" class="article-item">
-              <NuxtLink :to="article._path">
-                <div class="wrapper">
-                  <div class="img-cont h-72 mb-12 shrink-0">
-                    <nuxt-img :src="`/img/products/${article.images[0]}`" sizes="sm:100vw md:50vw lg:400px" :alt="article.title" class="" />
-                  </div>
-                  <header>
-                    
-                    <h2 class="text-2xl font-semibold">{{ article.title }}</h2>
-                    <p>{{ article.powerRange.from }} – {{ article.powerRange.to }} {{ article.powerRange.unit }}</p>
-                    <p>{{ article.description }}</p>
-                    <ul class="article-tags">
-                      <li class="tag !py-0.5" v-for="(tag, n) in article.category" :key="n">{{ tag }}</li>
-                    </ul>
-                  </header>
+            <!-- Render list of all articles in ./content/blog using `path` -->
+            <!-- Provide only defined fieldsin the `:query` prop -->
+            <ContentList
+              path="/product"
+              :query="{
+                only: ['title', 'description', 'category', '_path', 'highlights', 'powerRange', 'images', 'singlePhase' ],
+                $sensitivity: 'base',
+              }"
+            >
+              <!-- Default list slot -->
+              <template v-slot="{ list }">
+                <ul class="list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 | bg-gray-100 gap-x-px">
+                  <li v-for="article in list" :key="article._path" class="relative bg-white flex flex-col">
+                    <div class="p-6">
+                    <h2 class="text-h3 font-semibold mb-2">{{ article.title }}</h2>
+                    <dl class="flex flex-row gap-2">
+                    <div class="flex flex-row items-center gap-2">
+                      <dt class="sr-only">Power Range</dt>
+                      <dd class="c-pill | inline-flex px-2 py-1 bg-black text-white text-xs font-bold antialiased">{{ article.powerRange.from }} – {{ article.powerRange.to }} {{ article.powerRange.unit }}</dd>
+                    </div>
+                    <div class="flex flex-row items-center gap-2">
+                      <dt class="sr-only">Singlephase</dt>
+                      <dd class="c-pill | inline-flex px-2 py-1 bg-gray-700 text-white text-xs font-bold antialiased">{{ article.singlePhase }}</dd>
+                    </div>
+                  </dl>
                 </div>
-              </NuxtLink>
-            </li>
-          </ul>
-        </template>
 
-        <!-- Not found slot to display message when no content us is found -->
-        <template #not-found>
-          <p>No articles found.</p>
-        </template>
-      </ContentList>
-    </section>
+                  <!-- <ul class="article-tags">
+                    <li class="tag !py-0.5" v-for="(tag, n) in article.category" :key="n">{{ tag }}</li>
+                  </ul> -->
+
+                    <nuxt-img :src="`/img/products/${article.images[0]}`" sizes="sm:100vw md:50vw lg:400px" :alt="article.title" class="max-w-xs mx-auto pt-6 pb-10" />
+                    <NuxtLink :to="article._path" class="c-btn c-btn--primary px-6 py-4 | a11y-link | w-fit self-end inline-flex justify-content-between gap-4 no-underline" :aria-label="`View ${article.title}`">View Product <span aria-hidden="true">↗</span></NuxtLink>                      
+                  </li>
+                </ul>
+              </template>
+
+              <!-- Not found slot to display message when no content us is found -->
+              <template #not-found>
+                <p>No articles found.</p>
+              </template>
+            </ContentList>            
+
+          </div>
+
+        </section>
+
+      </article>      
   </main>
 </template>
