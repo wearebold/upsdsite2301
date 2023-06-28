e46eed962f709aec2b1bccaed8de76f1cd4397d8
Elliott.Evans <hello@wearebold.co.uk>
2023-06-28 09:21:49 +0100

    update(Logic, filter products)

--- site/pages/products/index.vue
+++ site/pages/products/index.vue
@@ -1,80 +1,204 @@
 <!-- ./pages/products/index.vue -->
 
 <script setup>
+import { ref, computed, onMounted } from "vue";
+
+// Meta data
 definePageMeta({
   key: (route) => route.fullPath,
+});
+
+// Filter state
+const selectedMaxPower = ref(0);
+const selectedCategories = ref([]);
+const selectedSubCategories = ref([]);
+
+// Fetch all products
+const articles = ref([]);
+
+onMounted(async () => {
+  // Fetch articles on component mount.
+  articles.value = await queryContent("products").find();
+  // Set the default value of selectedMaxPower to maxPowerRange
+  selectedMaxPower.value = maxPowerRange.value;
+
+  console.log(articles.value);
+});
+
+// Filtered products
+const filteredProducts = computed(() => {
+  return articles.value.filter((product) => {
+    // Convert power range to a common unit (e.g., VA)
+    let productMaxPower = product.powerRange.to;
+    if (product.powerRange.unit === "kVA") {
+      productMaxPower *= 1000;
+    }
+
+    // Check if product's max power is less than or equal to the selected max power
+    const withinPowerRange = productMaxPower <= selectedMaxPower.value;
+
+    const inSelectedCategory =
+      selectedCategories.value.length === 0 ||
+      product.category.some((cat) => selectedCategories.value.includes(cat));
+
+    const inSelectedSubCategory =
+      selectedSubCategories.value.length === 0 ||
+      selectedSubCategories.value.includes(product.subCategory);
+
+    return withinPowerRange && inSelectedCategory && inSelectedSubCategory;
+  });
+});
+
+// Max Power range
+const maxPowerRange = computed(() => {
+  return articles.value.reduce((max, product) => {
+    const productMaxPower =
+      product.powerRange.unit === "kVA"
+        ? product.powerRange.to * 1000
+        : product.powerRange.to;
+    return Math.max(max, productMaxPower);
+  }, 0);
+});
+
+// Categories
+const categories = computed(() => {
+  const allCategories = articles.value.flatMap((article) => article.category);
+  return [...new Set(allCategories)];
+});
+
+// Subcategories
+const subCategories = computed(() => {
+  const allSubCategories = articles.value.flatMap(
+    (article) => article.subCategory || []
+  );
+  return [...new Set(allSubCategories)];
 });
 
 // set meta for page
 useHead({
   title: "All products",
-  meta: [{ name: "description", content: "Here's a list of all my great products" }],
+  meta: [
+    { name: "description", content: "Here's a list of all my great products" },
+  ],
 });
 </script>
+
 <template>
-     <main class="">
-      <article class="l-container">
-        <header class="grid grid-cols-1 l-grid-line l-grid-line--t md:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px">
-          <div class="hidden md:block md:col-span-2 bg-white p-4">
-          </div>
-          <div class="md:col-span-10 bg-b-blue-500 text-white" style="padding: var(--space-xs);">
-            <h1 class="text-h1 font-medium leading-tight tracking-wide">Products</h1>
-          </div>
-        </header>
-        <section class="grid grid-cols-1 l-grid-line l-grid-line--t lg:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px">
-          <aside class="lg:col-span-2 bg-white p-4">
-            <!-- <Category /> -->
-            Filters
-          </aside>
-          <div class="lg:col-span-10 bg-white">          
+  <main class="">
+    <article class="l-container">
+      <header
+        class="grid grid-cols-1 l-grid-line l-grid-line--t md:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px"
+      >
+        <div class="hidden md:block md:col-span-2 bg-white p-4"></div>
+        <div
+          class="md:col-span-10 bg-b-blue-500 text-white"
+          style="padding: var(--space-xs)"
+        >
+          <h1 class="text-h1 font-medium leading-tight tracking-wide">
+            Products
+          </h1>
+        </div>
+      </header>
+      <section
+        class="grid grid-cols-1 l-grid-line l-grid-line--t lg:grid-cols-12 | relative bg-gray-100 gap-px pt-px px-px"
+      >
+        <aside class="lg:col-span-2 bg-white p-4">
+          <!-- <Category /> -->
+          <h2 class="text-h5 font-bold mb-4">Filters</h2>
 
-            <!-- Render list of all articles in ./content/blog using `path` -->
-            <!-- Provide only defined fieldsin the `:query` prop -->
-            <ContentList
-              path="/product"
-              :query="{
-                only: ['title', 'description', 'category', '_path', 'highlights', 'powerRange', 'images', 'singlePhase' ],
-                $sensitivity: 'base',
-              }"
+          <h3 class="text-h6 font-bold mb-4">Power Range</h3>
+          <label class="block mb-6">
+            <span class="block">Max Power (VA):</span>
+            <input
+              type="range"
+              min="0"
+              :max="maxPowerRange"
+              v-model="selectedMaxPower"
+            />
+            <span class="block">{{ selectedMaxPower }} VA</span>
+          </label>
+
+          <h3 class="text-h6 font-bold mb-4">Categories</h3>
+          <div class="mb-6">
+            <label class="block" v-for="category in categories" :key="category">
+              <input
+                type="checkbox"
+                :value="category"
+                v-model="selectedCategories"
+              />
+              {{ category }}
+            </label>
+          </div>
+
+          <h3 class="text-h6 font-bold mb-4">Subcategories</h3>
+          <div class="mb-6">
+            <label
+              class="block"
+              v-for="subCategory in subCategories"
+              :key="subCategory"
             >
-              <!-- Default list slot -->
-              <template v-slot="{ list }">
-                <ul class="list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 | bg-gray-100 gap-x-px">
-                  <li v-for="article in list" :key="article._path" class="relative bg-white flex flex-col">
-                    <div class="p-6">
-                    <h2 class="text-h3 font-semibold mb-2">{{ article.title }}</h2>
-                    <dl class="flex flex-row gap-2">
-                    <div class="flex flex-row items-center gap-2">
-                      <dt class="sr-only">Power Range</dt>
-                      <dd class="c-pill | inline-flex px-2 py-1 bg-black text-white text-xs font-bold antialiased">{{ article.powerRange.from }} – {{ article.powerRange.to }} {{ article.powerRange.unit }}</dd>
-                    </div>
-                    <div class="flex flex-row items-center gap-2">
-                      <dt class="sr-only">Singlephase</dt>
-                      <dd class="c-pill | inline-flex px-2 py-1 bg-gray-700 text-white text-xs font-bold antialiased">{{ article.singlePhase }}</dd>
-                    </div>
-                  </dl>
-                </div>
+              <input
+                type="checkbox"
+                :value="subCategory"
+                v-model="selectedSubCategories"
+              />
+              {{ subCategory }}
+            </label>
+          </div>
+        </aside>
+        <div class="lg:col-span-10 bg-white">
+          <ul
+            v-if="filteredProducts"
+            class="list-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 | bg-gray-100 gap-x-px"
+          >
+            <li
+              v-for="article in filteredProducts"
+              :key="article._path"
+              class="relative bg-white flex flex-col h-full"
+            >
+              <div class="p-6">
+                <h2 class="text-h3 font-semibold mb-2">{{ article.title }}</h2>
+                <dl class="flex flex-row gap-2">
+                  <div class="flex flex-row items-center gap-2">
+                    <dt class="sr-only">Power Range</dt>
+                    <dd
+                      class="c-pill | inline-flex px-2 py-1 bg-black text-white text-xs font-bold antialiased"
+                    >
+                      {{ article.powerRange.from }} –
+                      {{ article.powerRange.to }} {{ article.powerRange.unit }}
+                    </dd>
+                  </div>
+                  <div class="flex flex-row items-center gap-2">
+                    <dt class="sr-only">Singlephase</dt>
+                    <dd
+                      class="c-pill | inline-flex px-2 py-1 bg-gray-700 text-white text-xs font-bold antialiased"
+                    >
+                      {{ article.singlePhase }}
+                    </dd>
+                  </div>
+                </dl>
+              </div>
 
-                  <!-- <ul class="article-tags">
+              <!-- <ul class="article-tags">
                     <li class="tag !py-0.5" v-for="(tag, n) in article.category" :key="n">{{ tag }}</li>
                   </ul> -->
 
-                    <nuxt-img :src="`/img/products/${article.images[0]}`" sizes="sm:100vw md:50vw lg:400px" :alt="article.title" class="max-w-xs mx-auto pt-6 pb-10" />
-                    <NuxtLink :to="article._path" class="c-btn c-btn--primary px-6 py-4 | a11y-link | w-fit self-end inline-flex justify-content-between gap-4 no-underline" :aria-label="`View ${article.title}`">View Product <span aria-hidden="true">↗</span></NuxtLink>                      
-                  </li>
-                </ul>
-              </template>
-
-              <!-- Not found slot to display message when no content us is found -->
-              <template #not-found>
-                <p>No articles found.</p>
-              </template>
-            </ContentList>            
-
-          </div>
-
-        </section>
-
-      </article>      
+              <nuxt-img
+                :src="`/img/products/${article.images[0]}`"
+                sizes="sm:100vw md:50vw lg:400px"
+                :alt="article.title"
+                class="max-w-xs max-h-96 mx-auto pt-6 pb-10 object-contain"
+              />
+              <NuxtLink
+                :to="article._path"
+                class="c-btn c-btn--primary px-6 py-4 mt-auto | a11y-link | w-fit self-end inline-flex justify-content-between gap-4 no-underline"
+                :aria-label="`View ${article.title}`"
+                >View Product <span aria-hidden="true">↗</span></NuxtLink
+              >
+            </li>
+          </ul>
+        </div>
+      </section>
+    </article>
   </main>
 </template>
