6747ca66a780fd45d39a3b0cda6846c96f6328d8
Elliott.Evans <hello@wearebold.co.uk>
2023-06-28 12:53:27 +0100

    update(Form)

--- site/components/ContactForm.vue
+++ site/components/ContactForm.vue
@@ -1,24 +1,31 @@
 <template>
-    <form data-netlify="true">
-      <div>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" required>
-      </div>
-      <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
-      </div>
-      <div>
-        <label for="message">Message</label>
-        <textarea id="message" name="message" required></textarea>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </template>
-  
-  <script>
-  export default {
-    // Component logic
-  }
-  </script>
-  
+  <form name="contact" method="POST" action="/contact" data-netlify="true">
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label
+        >Your Role:
+        <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label
+      >
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+</template>
+
+<script>
+export default {
+  // Component logic
+};
+</script>
